be604ff693af3183a252bba3bc7976254aa57f34
ErikManv <erikmanvelyan@yandex.ru>
2023-04-14 18:55:09 +0400

    5 commit. Check app

--- stats/client/src/main/java/ru/practicum/statsclient/StatsClient.java
+++ stats/client/src/main/java/ru/practicum/statsclient/StatsClient.java
@@ -28,7 +28,7 @@ public class StatsClient extends BaseClient {
         Map<String, Object> parameters = Map.of(
                 "start", start,
                 "end", end,
-                "urdfis", uris,
+                "uris", uris,
                 "unique", unique
         );
         return get("/stats?start={start}&end={end}&uris={uris}&unique={unique}", parameters);
