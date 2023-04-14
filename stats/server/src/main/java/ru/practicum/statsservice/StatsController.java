524c39af81479045591a7e29477cf4720889fec2
ErikManv <erikmanvelyan@yandex.ru>
2023-04-14 18:45:58 +0400

    5 commit. Check app

--- stats/server/src/main/java/ru/practicum/statsservice/StatsController.java
+++ stats/server/src/main/java/ru/practicum/statsservice/StatsController.java
@@ -2,6 +2,8 @@ package ru.practicum.statsservice;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.statsdto.HitDtoInput;
 import ru.practicum.statsdto.HitDtoOut;
@@ -21,21 +23,21 @@ public class StatsController {
     }
 
     @GetMapping("/stats")
-    public List<HitDtoOut> stats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-                                    @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
-                                    @RequestParam(required = false) List<String> uris,
-                                    @RequestParam(defaultValue = "false", required = false) boolean unique) {
+    public ResponseEntity<List<HitDtoOut>> stats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                                @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                                @RequestParam(required = false) List<String> uris,
+                                                @RequestParam(defaultValue = "false", required = false) boolean unique) {
         if (unique) {
             if (uris == null) {
-                return statsRepository.getStatisticsUniq(start, end);
+                return new ResponseEntity<>(statsRepository.getStatisticsUniq(start, end), HttpStatus.OK);
             } else {
-                return statsRepository.getStatisticsUniqAndUris(start, end, uris);
+                return new ResponseEntity<>(statsRepository.getStatisticsUniqAndUris(start, end, uris), HttpStatus.OK);
             }
         } else {
             if (uris == null) {
-                return statsRepository.getStatistics(start, end);
+                return new ResponseEntity<>(statsRepository.getStatistics(start, end), HttpStatus.OK);
             } else {
-                return statsRepository.getStatisticsUris(start, end, uris);
+                return new ResponseEntity<>(statsRepository.getStatisticsUris(start, end, uris), HttpStatus.OK);
             }
         }
     }
