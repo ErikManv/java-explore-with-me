653dce80acb85bfa714e4812abba52cf1982fbf7
ErikManv <erikmanvelyan@yandex.ru>
2023-04-16 01:03:17 +0400

    13 commit. all fixed

--- stats/server/src/main/java/ru/practicum/statsserver/HitMapper.java
+++ stats/server/src/main/java/ru/practicum/statsserver/HitMapper.java
@@ -1,11 +1,10 @@
 package ru.practicum.statsserver;
 
-import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import ru.practicum.statsdto.HitDtoInput;
 
-@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Mapper(componentModel = "spring")
 public interface HitMapper {
     @Mapping(source = "timestamp",target = "timestamp", dateFormat = "yyyy-MM-dd HH:mm:ss")
     Hit toHit(HitDtoInput hitDtoInput);
