53e64f5a9bdb3d54d2554c828f9cd77595ae2082
ErikManv <erikmanvelyan@yandex.ru>
2023-04-28 03:03:12 +0400

    5 commit. Second part

--- ewm/src/main/java/ru/practicum/exceptions/DeleteObjectInUseException.java
+++ ewm/src/main/java/ru/practicum/exceptions/DeleteObjectInUseException.java
@@ -2,7 +2,7 @@ package ru.practicum.exceptions;
 
 public class DeleteObjectInUseException extends RuntimeException {
 
-    public DeleteObjectInUseException (String className, String email) {
+    public DeleteObjectInUseException(String className, String email) {
         super(String.format("%s - %s in use", className, email));
     }
 }
